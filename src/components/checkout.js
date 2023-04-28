5263572a289e2731d6ccc9c428e2dd23c6b6009c
Akhileswar Reddy <119875281+AkhilReddy3512@users.noreply.github.com>
2023-04-29 02:37:18 +0530

    major ui update

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -16,7 +16,7 @@ const month = oneYearFromNow.getMonth() + 1;
 const day = oneYearFromNow.getDate();
 const year = oneYearFromNow.getFullYear();
 const formattedDate = `${month.toString().padStart(2, '0')}/${day.toString().padStart(2, '0')}/${year}`;
-localStorage.setItem('Products',JSON.stringify([]))
+localStorage.setItem('Products', JSON.stringify([]))
 
 function loadScript(src) {
     return new Promise((resolve) => {
@@ -33,10 +33,10 @@ function loadScript(src) {
 }
 
 function Checkout() {
-    
-const [open1, setOpen1] = useState(false);
-const handleOpen = () => setOpen1(true);
-const handleClose = () => setOpen1(false);
+
+    const [open1, setOpen1] = useState(false);
+    const handleOpen = () => setOpen1(true);
+    const handleClose = () => setOpen1(false);
     const [products] = useState([
         { id: 1, name: "Product1", amount: 100 },
         { id: 2, name: "Product2", amount: 100 },
@@ -45,7 +45,7 @@ const handleClose = () => setOpen1(false);
         { id: 5, name: "Product5", amount: 100 },
     ]);
 
-   // localStorage.setItem("Products", JSON.stringify([]))
+    // localStorage.setItem("Products", JSON.stringify([]))
 
     const [cartItems, setCartItems] = useState([]);
     const [totalAmount, setTotalAmount] = useState(0);
@@ -131,11 +131,11 @@ const handleClose = () => setOpen1(false);
     };
     const navigate = useNavigate();
 
-    const handleSubmit=async(event)=>{
+    const handleSubmit = async (event) => {
         event.preventDefault();
         setPhoneSubmitted(true);
         console.log('Submitting phone number: ', phoneNumber);
-    serachrecord(phoneNumber)
+        serachrecord(phoneNumber)
     }
 
     async function displayRazorpay() {
@@ -164,8 +164,8 @@ const handleClose = () => setOpen1(false);
             image: 'https://www.arm.com/-/media/global/Why%20Arm/partner/Partner%20Ecosystem/ai-catalog/reality-ai/reality-ai-logo.png?rev=03b4e9975e1540cf8141d72d011981f0&revision=03b4e997-5e15-40cf-8141-d72d011981f0',
 
             handler: async function (response) {
-            
-                if ((JSON.parse(localStorage.getItem('Products')).length!==0)) {
+
+                if ((JSON.parse(localStorage.getItem('Products')).length !== 0)) {
                     for (var i = 0; i < cartItems.length; i++) {
                         label[cartItems[i].name] = formattedDate;
                     }
@@ -219,92 +219,93 @@ const handleClose = () => setOpen1(false);
         bgcolor: 'background.paper',
         border: '2px solid #000',
         boxShadow: 24,
-         p: 4,
-      };
+        p: 4,
+    };
     return (
         <>
-        <nav class="navbar sticky navbar-light bg-light justify-content-between">
-    <href class="text1 text3"><b>RealtyAi</b></href>
-    </nav>
-    <div style={{margin:"1rem"}} class="text-center">
-  <div class="row">
-    <div class="col">
-    <center>
-        <img style={{marginTop:"10vw",marginBottom:"2vw", height:"30vw", }} src="Group13393.jpg" alt="buy now"/>
-      </center>
-    </div>
-    <div style={{backgroundColor:"rgb(62, 196, 249)",color:"white"}} class="col">
-    <div>
-        <div class="line"></div>
-            <ToastContainer autoClose={4000} position="top-center" pauseOnHover={false} closeOnClick theme="colored" />
-            <div className='container-main'>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Phone Number: 
-                        <input type="tel" style={{margin:"2vw"}} value={phoneNumber} onChange={(event) => setPhoneNumber(event.target.value)} />
-                        {/* <input type="tel" /> */}
-                    </label>
-                    <button className="btn btn-outline-light" type="submit">Submit</button>
-                </form>
-                <ul>
-                <h2>Available Products</h2>  
-
-    {products.map((product) => (
-
-<div class="dropdown" key={product.id}>
-<button style={{width:"80%", marginLeft:"0px"}} class="btn btn-outline-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-{product.name}
-  </button>
-  <ul style={{width:"80%"}} class="dropdown-menu">
-    <li><div class="dropdown-item" >{product.name} - ₹ {product.amount}</div></li>
-    <li><div class="dropdown-item" >{phoneSubmitted && <button style={{width:"100%",marginLeft:"0px"}} className="btn btn-outline-info"  onClick={() => addToCart(product)}>Add to Package</button>}</div></li>
-  </ul>
- </div>
-                    ))}  
-                </ul>
-      <button style={{width:"50%"}} className="btn btn-light"  onClick={handleOpen}>View Cart</button>
-      <Modal
-        open={open1}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box style={{ borderRadius:"5%", backgroundColor:"rgb(3, 129, 178)", color:"white",borderColor:"white" }} sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-          <h5>My Package</h5>
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          {cartItems.length === 0 ? (
-                    <p>No items in your package, Check our products</p>
-                ) : (
-                    <ul>
-                        {cartItems.map((item) => (
-                            <li key={item.id}>
-                                {item.name} - ₹ {item.amount}
-                                <button className="btn btn-outline-light" onClick={() => removeCartItem(item)}>Remove</button>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-          </Typography>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            <hr/>
-                {totalAmount > 0 &&
-                    <div>
-                        <p>Total amount: ₹ {totalAmount}</p>
-                        <center><button style={{width:"90%", paddingLeft:"0px"}} className="btn btn-outline-light" onClick={displayRazorpay}>Proceed to Payment</button></center>
+            <nav class="navbar sticky navbar-light bg-light justify-content-between">
+                <href class="text1 text3"><b>RealtyAi</b></href>
+            </nav>
+            <div style={{ margin: "1rem" }} class="text-center">
+                <div class="row">
+                    <div class="col">
+                        <center>
+                            <img style={{ marginTop: "10vw", marginBottom: "2vw", height: "65vw", maxHeight:"425px"}} src="Group13393.jpg" alt="buy now" />
+                        </center>
                     </div>
-                }
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
-            </div>
-        </div>
-    </div>
-  </div>
+                    <div style={{ backgroundColor: "rgb(62, 196, 249)", color: "white" }} class="col">
+                        <div>
+                            <div class="line"></div>
+                            <ToastContainer autoClose={4000} position="top-center" pauseOnHover={false} closeOnClick theme="colored" />
+                            <div className='container-main'>
+                            <h4 style={{ textAlign:"left",marginLeft:"15vw"}}> Enter Mobile Number</h4>
+                                <form onSubmit={handleSubmit}>
+                                    <label>
+                                    
+                                        <input type="tel" style={{ margin: "2vw" }} value={phoneNumber} onChange={(event) => setPhoneNumber(event.target.value)} />
+                                        {/* <input type="tel" /> */}
+                                    </label>
+                                    <button className="btn btn-outline-light" type="submit">Submit</button>
+                                </form>
+                                <ul>
+                                    <h2>Available Products</h2>
 
-  </>
+                                    {products.map((product) => (
+
+                                        <div class="dropdown" key={product.id}>
+                                            <button style={{ width: "50%", margin: "3px" }} class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                {product.name}
+                                            </button>
+                                            <ul style={{ width: "50%" }} class="dropdown-menu">
+                                                <li><div class="dropdown-item" >{product.name} - ₹ {product.amount}</div></li>
+                                                <li><div class="dropdown-item" >{phoneSubmitted && <button style={{ width: "100%", marginLeft: "0px" }} className="btn btn-outline-info" onClick={() => addToCart(product)}>Add to Package</button>}</div></li>
+                                            </ul>
+                                        </div>
+                                    ))}
+                                </ul>
+                                <button style={{ width: "60%" , marginLeft:"4%", marginTop:"2vw", marginBottom:"6vw"}} className="btn btn-light" onClick={handleOpen}>View Cart</button>
+                                <Modal
+                                    open={open1}
+                                    onClose={handleClose}
+                                    aria-labelledby="modal-modal-title"
+                                    aria-describedby="modal-modal-description"
+                                >
+                                    <Box style={{ borderRadius: "5%", backgroundColor: "rgb(3, 129, 178)", color: "white", borderColor: "white" }} sx={style}>
+                                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                                            <h5>My Package</h5>
+                                        </Typography>
+                                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                            {cartItems.length === 0 ? (
+                                                <p>No items in your package, Check our products</p>
+                                            ) : (
+                                                <ul>
+                                                    {cartItems.map((item) => (
+                                                        <li key={item.id}>
+                                                            {item.name} - ₹ {item.amount}
+                                                            <button className="btn btn-outline-light" style={{marginLeft:"3%"}} onClick={() => removeCartItem(item)}>Remove</button>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            )}
+                                        </Typography>
+                                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                                            <hr />
+                                            {totalAmount > 0 &&
+                                                <div>
+                                                    <p>Total amount: ₹ {totalAmount}</p>
+                                                    <center><button style={{ width: "90%", paddingLeft: "0px" }} className="btn btn-outline-light" onClick={displayRazorpay}>Proceed to Payment</button></center>
+                                                </div>
+                                            }
+                                        </Typography>
+                                    </Box>
+                                </Modal>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
     )
 }
 
